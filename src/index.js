556629e46c31084427c33a0102564da6f58781f7
Maxime PEAN <mpean@restlet.com>
2017-11-12 22:03:04 +0100

    init simple content

--- src/index.js
+++ src/index.js
@@ -6,41 +6,31 @@ import './index.css';
 
 var content = {
   'bienvenue': {
-    'fr': 'Bienvenue Maxime en Français',
-    'en': 'Welcome Maxime in English'
+    'en': 'TODO',
+    'fr': 'Bienvenue chez Trust in Move',
+    'de': 'TODO',
+    'sp': 'TODO',
   },
-  'texte': {
-    'fr': 'Ceci est un texte.',
-    'en': 'This is a text.'
+  'construction': {
+    'en': 'TODO',
+    'fr': 'Ce site est en construction',
+    'de': 'TODO',
+    'sp': 'TODO',
   },
-  'lorem': {
-    'fr': 'Trust in Move accompagne les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement. Parce que nous vivons dans un monde en mouvement et savoir en tirer bénéfice est une des clefs du succès. ',
-    'en': 'Mauris vitae bibendum arcu. Donec et mi rutrum, euismod felis vel, varius sem. Phasellus efficitur rhoncus purus in suscipit. Integer quis blandit est, eu eleifend est. Sed molestie purus ex, sed sagittis leo aliquam quis. Aenean et tincidunt orci, vel efficitur mi. Vestibulum placerat mi diam, eu lacinia augue posuere et. Maecenas dictum ullamcorper aliquam. Sed non ligula quam. Suspendisse libero augue, ullamcorper ac placerat vitae, dignissim ac mauris.'
+  'intro': {
+    'en': 'TODO',
+    'fr': 'Trust in Move accompagne les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement. Parce' +
+    ' que nous vivons dans un monde en mouvement et savoir en tirer bénéfice est une des clefs du succès.',
+    'de': 'Trust in Move begleitet Unternehmen in die Welt von morgen: Internationalisierung, Export, Vertriebseffizienz, Veränderungsmanagement. Weil wir' +
+    ' in einer Welt in Bewegung leben, und das Beste daraus zu ziehen führt uns zum Erfolg!',
+    'sp': 'TODO'
   },
-  'bienvenue2': {
-    'fr': 'Bienvenue Maxime en Français',
-    'en': 'Welcome Maxime in English'
-  },
-  'texte2': {
-    'fr': 'Ceci est un texte.',
-    'en': 'This is a text.'
-  },
-  'lorem2': {
-    'fr': 'Trust in Move accompagne les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement. Parce que nous vivons dans un monde en mouvement et savoir en tirer bénéfice est une des clefs du succès. ',
-    'en': 'Mauris vitae bibendum arcu. Donec et mi rutrum, euismod felis vel, varius sem. Phasellus efficitur rhoncus purus in suscipit. Integer quis blandit est, eu eleifend est. Sed molestie purus ex, sed sagittis leo aliquam quis. Aenean et tincidunt orci, vel efficitur mi. Vestibulum placerat mi diam, eu lacinia augue posuere et. Maecenas dictum ullamcorper aliquam. Sed non ligula quam. Suspendisse libero augue, ullamcorper ac placerat vitae, dignissim ac mauris.'
-  },
-  'bienvenue3': {
-    'fr': 'Bienvenue Maxime en Français',
-    'en': 'Welcome Maxime in English'
-  },
-  'texte3': {
-    'fr': 'Ceci est un texte.',
-    'en': 'This is a text.'
-  },
-  'lorem3': {
-    'fr': 'Trust in Move accompagne les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement. Parce que nous vivons dans un monde en mouvement et savoir en tirer bénéfice est une des clefs du succès. ',
-    'en': 'Mauris vitae bibendum arcu. Donec et mi rutrum, euismod felis vel, varius sem. Phasellus efficitur rhoncus purus in suscipit. Integer quis blandit est, eu eleifend est. Sed molestie purus ex, sed sagittis leo aliquam quis. Aenean et tincidunt orci, vel efficitur mi. Vestibulum placerat mi diam, eu lacinia augue posuere et. Maecenas dictum ullamcorper aliquam. Sed non ligula quam. Suspendisse libero augue, ullamcorper ac placerat vitae, dignissim ac mauris.'
-  },
+  'contact': {
+    'en': 'TODO',
+    'fr': 'Contactez-nous pour plus d’informations : contact@trustinmove.com',
+    'de': 'Kontaktieren Sie uns für mehr Informationen: contact@trustinmove.com',
+    'sp': 'TODO',
+  }
 };
 
 var lang = 'en';
