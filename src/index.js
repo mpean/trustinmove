a10b04d9be2b286ef50922ee59a27cd202cdeedf
Maxime PEAN <mpean@restlet.com>
2017-11-18 15:55:08 +0100

    improve basic content

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 
 "use strict";
 
-var content = {
+let content = {
   'bienvenue': {
-    'en': 'TODO',
-    'fr': 'Bienvenue chez Trust in Move',
-    'de': 'TODO',
-    'sp': 'TODO',
-  },
-  'construction': {
-    'en': 'TODO',
-    'fr': 'Ce site est en construction',
-    'de': 'TODO',
+    'en': 'Welcome to Trust in Move!',
+    'fr': 'Bienvenue chez Trust in Move !',
+    'de': 'Willkommen bei Trust in Move!',
     'sp': 'TODO',
   },
   'intro': {
-    'en': 'TODO',
-    'fr': 'Trust in Move accompagne les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement. Parce' +
+    'en': 'Trust in Move helps organizations in moving towards future: internationalization, export, sales efficiency, change management.<br><br>Because we' +
+    ' live' +
+    ' in ' +
+    'a moving world and enjoying the most of it leads to success!',
+    'fr': 'Trust in Move accompagne les entreprises vers demain: internationalisation, export, efficacité commerciale, facilitation du' +
+    ' changement.<br><br>Parce' +
     ' que nous vivons dans un monde en mouvement et savoir en tirer bénéfice est une des clefs du succès.',
-    'de': 'Trust in Move begleitet Unternehmen in die Welt von morgen: Internationalisierung, Export, Vertriebseffizienz, Veränderungsmanagement. Weil wir' +
+    'de': 'Trust in Move begleitet Unternehmen in die Welt von morgen: Internationalisierung, Export, Vertriebseffizienz, Veränderungsmanagement.<br><br>Weil' +
+    ' wir' +
     ' in einer Welt in Bewegung leben, und das Beste daraus zu ziehen führt uns zum Erfolg!',
     'sp': 'TODO'
   },
   'contact': {
-    'en': 'TODO',
-    'fr': 'Contactez-nous pour plus d’informations : contact@trustinmove.com',
-    'de': 'Kontaktieren Sie uns für mehr Informationen: contact@trustinmove.com',
+    'en': 'Contact us for more details:',
+    'fr': 'Contactez-nous pour plus d’informations :',
+    'de': 'Kontaktieren Sie uns für mehr Informationen:',
     'sp': 'TODO',
   }
 };
 
-var lang = 'en';
+let lang = 'en';
 
 function displayLanguage (language) {
   lang = language;
