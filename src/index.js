7e3e212f0aa07a281de1f581844218354939d77a
Maxime PEAN <mpean@restlet.com>
2018-11-11 16:21:27 +0100

    add consulting and formation

--- src/index.js
+++ src/index.js
@@ -1,39 +1,119 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './bootstrap';
-import 'font-awesome/css/font-awesome.min.css';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./bootstrap";
+import "font-awesome/css/font-awesome.min.css";
+import "./index.css";
 
 "use strict";
 
 let content = {
-  'bienvenue': {
-    'en': 'Welcome to Trust in Move!',
-    'fr': 'Bienvenue chez Trust in Move !',
-    'de': 'Willkommen bei Trust in Move!',
-    'sp': 'TODO',
+  "acceuilTitle": {
+    "en": "Pourquoi Trust in Move ?",
+    "fr": "Pourquoi Trust in Move ?",
+    "de": "Pourquoi Trust in Move ?",
+    "sp": "Pourquoi Trust in Move ?",
   },
-  'intro': {
-    'en': 'Trust in Move helps organizations in moving towards future: internationalization, export, sales efficiency, change management.<br><br>Because we' +
-    ' live' +
-    ' in ' +
-    'a moving world and enjoying the most of it leads to success!',
-    'fr': 'Trust in Move accompagne les entreprises vers demain: internationalisation, export, efficacité commerciale, facilitation du' +
-    ' changement.<br><br>Parce' +
-    ' que nous vivons dans un monde en mouvement et savoir en tirer bénéfice est une des clefs du succès.',
-    'de': 'Trust in Move begleitet Unternehmen in die Welt von morgen: Internationalisierung, Export, Vertriebseffizienz, Veränderungsmanagement.<br><br>Weil' +
-    ' wir' +
-    ' in einer Welt in Bewegung leben, und das Beste daraus zu ziehen führt uns zum Erfolg!',
-    'sp': 'TODO'
+  "acceuilTexte1": {
+    "en": "Parce que nous vivons dans un monde en mouvement et que savoir en tirer bénéfice est une des clefs du succès. Trust in Move accompagne donc les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement.",
+    "fr": "Parce que nous vivons dans un monde en mouvement et que savoir en tirer bénéfice est une des clefs du succès. Trust in Move accompagne donc les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement.",
+    "de": "Parce que nous vivons dans un monde en mouvement et que savoir en tirer bénéfice est une des clefs du succès. Trust in Move accompagne donc les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement.",
+    "sp": "Parce que nous vivons dans un monde en mouvement et que savoir en tirer bénéfice est une des clefs du succès. Trust in Move accompagne donc les entreprises vers demain : internationalisation, export, efficacité commerciale, facilitation du changement."
   },
-  'contact': {
-    'en': 'Contact us for more details:',
-    'fr': 'Contactez-nous pour plus d’informations :',
-    'de': 'Kontaktieren Sie uns für mehr Informationen:',
-    'sp': 'TODO',
+  "acceuilQuestion": {
+    "en": "Que peut Trust in Move pour moi ?",
+    "fr": "Que peut Trust in Move pour moi ?",
+    "de": "Que peut Trust in Move pour moi ?",
+    "sp": "Que peut Trust in Move pour moi ?"
+  },
+  "acceuilTexte2": {
+    "en": "Développer mon entreprise à l’international.",
+    "fr": "Développer mon entreprise à l’international.",
+    "de": "Développer mon entreprise à l’international.",
+    "sp": "Développer mon entreprise à l’international."
+  },
+  "acceuilTexte3": {
+    "en": "Formation à l’internationalisation des PME.",
+    "fr": "Formation à l’internationalisation des PME.",
+    "de": "Formation à l’internationalisation des PME.",
+    "sp": "Formation à l’internationalisation des PME."
+  },
+
+  "a1Title": {
+    "en": "Conseil stratégie et opérationnel export",
+    "fr": "Conseil stratégie et opérationnel export",
+    "de": "Conseil stratégie et opérationnel export",
+    "sp": "Conseil stratégie et opérationnel export"
+  },
+  "a1Texte1": {
+    "en": "Trust in Move vous facilite l'international.<br>Que ce soit pour du <strong>conseil stratégique</strong> ou un audit, de <strong>l'appui opérationnel</strong> sur vos marchés, de l'externalisation de votre équipe commerciale ou pour vous aider à <strong>choisir le bon partenaire à l'étranger</strong> (qui allie compétences techniques et efficacité commerciale), Trust in Move est là pour vous.",
+    "fr": "Trust in Move vous facilite l'international.<br>Que ce soit pour du <strong>conseil stratégique</strong> ou un audit, de <strong>l'appui opérationnel</strong> sur vos marchés, de l'externalisation de votre équipe commerciale ou pour vous aider à <strong>choisir le bon partenaire à l'étranger</strong> (qui allie compétences techniques et efficacité commerciale), Trust in Move est là pour vous.",
+    "de": "Trust in Move vous facilite l'international.<br>Que ce soit pour du <strong>conseil stratégique</strong> ou un audit, de <strong>l'appui opérationnel</strong> sur vos marchés, de l'externalisation de votre équipe commerciale ou pour vous aider à <strong>choisir le bon partenaire à l'étranger</strong> (qui allie compétences techniques et efficacité commerciale), Trust in Move est là pour vous.",
+    "sp": "Trust in Move vous facilite l'international.<br>Que ce soit pour du <strong>conseil stratégique</strong> ou un audit, de <strong>l'appui opérationnel</strong> sur vos marchés, de l'externalisation de votre équipe commerciale ou pour vous aider à <strong>choisir le bon partenaire à l'étranger</strong> (qui allie compétences techniques et efficacité commerciale), Trust in Move est là pour vous."
+  },
+  "a1Texte2": {
+    "en": "Basé à Nantes, Trust in Move travaille pour des <strong>PME industrielles</strong> françaises qui cherchent à s'internationaliser (Europe, Amériques, Asie, Moyen Orient…) et aide aussi des entreprises étrangères à trouver les bons partenaires en France.",
+    "fr": "Basé à Nantes, Trust in Move travaille pour des <strong>PME industrielles</strong> françaises qui cherchent à s'internationaliser (Europe, Amériques, Asie, Moyen Orient…) et aide aussi des entreprises étrangères à trouver les bons partenaires en France.",
+    "de": "Basé à Nantes, Trust in Move travaille pour des <strong>PME industrielles</strong> françaises qui cherchent à s'internationaliser (Europe, Amériques, Asie, Moyen Orient…) et aide aussi des entreprises étrangères à trouver les bons partenaires en France.",
+    "sp": "Basé à Nantes, Trust in Move travaille pour des <strong>PME industrielles</strong> françaises qui cherchent à s'internationaliser (Europe, Amériques, Asie, Moyen Orient…) et aide aussi des entreprises étrangères à trouver les bons partenaires en France."
+  },
+  "a1Texte3": {
+    "en": "Notre plus-value est l'accompagnement à la valorisation commerciale de produits techniques.<br>Nous intervenons quand vous le décidez et à votre rythme.",
+    "fr": "Notre plus-value est l'accompagnement à la valorisation commerciale de produits techniques.<br>Nous intervenons quand vous le décidez et à votre rythme.",
+    "de": "Notre plus-value est l'accompagnement à la valorisation commerciale de produits techniques.<br>Nous intervenons quand vous le décidez et à votre rythme.",
+    "sp": "Notre plus-value est l'accompagnement à la valorisation commerciale de produits techniques.<br>Nous intervenons quand vous le décidez et à votre rythme."
+  },
+  "a1Contact": {
+    "en": "Contactez-nous aujourd'hui pour en savoir plus !",
+    "fr": "Contactez-nous aujourd'hui pour en savoir plus !",
+    "de": "Contactez-nous aujourd'hui pour en savoir plus !",
+    "sp": "Contactez-nous aujourd'hui pour en savoir plus !"
+  },
+
+  "a2Title": {
+    "en": "Formation à l'international",
+    "fr": "Formation à l'international",
+    "de": "Formation à l'international",
+    "sp": "Formation à l'international"
+  },
+  "a2Texte1": {
+    "en": "Trust in Move forme dans tous les aspects spécifiques de la vente à l'international : <strong>négociation</strong> dans un contexte <strong>interculturel</strong>, techniques administratives export, management commercial, fondamentaux de <strong>l'export</strong>…",
+    "fr": "Trust in Move forme dans tous les aspects spécifiques de la vente à l'international : <strong>négociation</strong> dans un contexte <strong>interculturel</strong>, techniques administratives export, management commercial, fondamentaux de <strong>l'export</strong>…",
+    "de": "Trust in Move forme dans tous les aspects spécifiques de la vente à l'international : <strong>négociation</strong> dans un contexte <strong>interculturel</strong>, techniques administratives export, management commercial, fondamentaux de <strong>l'export</strong>…",
+    "sp": "Trust in Move forme dans tous les aspects spécifiques de la vente à l'international : <strong>négociation</strong> dans un contexte <strong>interculturel</strong>, techniques administratives export, management commercial, fondamentaux de <strong>l'export</strong>…"
+  },
+  "a2Texte2": {
+    "en": "Nos formations <strong>sur mesure</strong> s'adressent à tous les publics d'apprenants qui travaillent dans un contexte international : commerciaux, managers, administratifs, chefs de projets…",
+    "fr": "Nos formations <strong>sur mesure</strong> s'adressent à tous les publics d'apprenants qui travaillent dans un contexte international : commerciaux, managers, administratifs, chefs de projets…",
+    "de": "Nos formations <strong>sur mesure</strong> s'adressent à tous les publics d'apprenants qui travaillent dans un contexte international : commerciaux, managers, administratifs, chefs de projets…",
+    "sp": "Nos formations <strong>sur mesure</strong> s'adressent à tous les publics d'apprenants qui travaillent dans un contexte international : commerciaux, managers, administratifs, chefs de projets…"
+  },
+  "a2Texte3": {
+    "en": "Adepte de modalités pédagogiques innovantes (blended learning, jeux pédagogiques, …) et revisitant régulièrement ses supports pour toujours être à la pointe, Trust in Move développe des formations de qualité pour ses clients.",
+    "fr": "Adepte de modalités pédagogiques innovantes (blended learning, jeux pédagogiques, …) et revisitant régulièrement ses supports pour toujours être à la pointe, Trust in Move développe des formations de qualité pour ses clients.",
+    "de": "Adepte de modalités pédagogiques innovantes (blended learning, jeux pédagogiques, …) et revisitant régulièrement ses supports pour toujours être à la pointe, Trust in Move développe des formations de qualité pour ses clients.",
+    "sp": "Adepte de modalités pédagogiques innovantes (blended learning, jeux pédagogiques, …) et revisitant régulièrement ses supports pour toujours être à la pointe, Trust in Move développe des formations de qualité pour ses clients."
+  },
+  "a2Texte4": {
+    "en": "Toutes nos prestations peuvent être réalisées entièrement en Français, Anglais, Allemand ou Espagnol.",
+    "fr": "Toutes nos prestations peuvent être réalisées entièrement en Français, Anglais, Allemand ou Espagnol.",
+    "de": "Toutes nos prestations peuvent être réalisées entièrement en Français, Anglais, Allemand ou Espagnol.",
+    "sp": "Toutes nos prestations peuvent être réalisées entièrement en Français, Anglais, Allemand ou Espagnol."
+  },
+  "a2Contact": {
+    "en": "Contactez-nous dès aujourd'hui pour en savoir plus !",
+    "fr": "Contactez-nous dès aujourd'hui pour en savoir plus !",
+    "de": "Contactez-nous dès aujourd'hui pour en savoir plus !",
+    "sp": "Contactez-nous dès aujourd'hui pour en savoir plus !"
+  },
+
+  "contact": {
+    "en": "Contact us for more details:",
+    "fr": "Contactez-nous pour plus d’informations :",
+    "de": "Kontaktieren Sie uns für mehr Informationen:",
+    "sp": "TODO",
   }
 };
 
-let lang = 'en';
+let lang = "en";
 
 function displayLanguage (language) {
   lang = language;
